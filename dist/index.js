c8150eeb9ebeaba352f1bf0b4d62cd596f0cb916
robi <robert.szaloki@euedge.com>
2019-04-18 13:37:32 +0200

    remove dist from repo

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Augmint_1 = require("./Augmint");
-var index_1 = require("./utils/index");
-exports.default = {
-    Augmint: Augmint_1.default,
-    utils: index_1.default
-};
-//# sourceMappingURL=index.js.map
--- dist/utils/log.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ulog = require("ulog");
-var log = function (moduleName) { return ulog(moduleName); };
-exports.default = log;
-//# sourceMappingURL=log.js.map
