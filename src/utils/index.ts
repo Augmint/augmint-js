8e8569e1e1a49b2ce001c186e60d4c73bb63c136
szerintedmi <szerintedmi@gmail.com>
2019-04-19 12:24:17 +0100

    logger import fix (revert to require)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 export * from "./sigintHandler";
 export * from "./promiseTimeout";
 export * from "./env";
-import logger from "ulog";
+import logger = require("ulog"); // logger doesn't work with import (transpiled code fails)
 
 export { logger };
