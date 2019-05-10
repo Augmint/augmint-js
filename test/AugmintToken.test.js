cc18983f3764ebf19bb66ed0b37246e41e364831
Peter Petrovics <peter@augmint.org>
2019-05-10 12:28:58 +0100

    Legacy contract connections (#51)

--- test/AugmintToken.test.js
+++ test/AugmintToken.test.js
@@ -11,14 +11,25 @@ if (config.LOG) {
 }
 
 describe("AugmintToken connection", () => {
+    let augmint;
+    before(async () => {
+        augmint = await Augmint.create(config);
+    });
+
     it("should connect to latest contract", async () => {
-        const myAugmint = await Augmint.create(config);
-
-        const peggedSymbol = await myAugmint.token.peggedSymbol;
-
+        const peggedSymbol = await augmint.token.peggedSymbol;
 
         assert.equal(peggedSymbol, CCY);
     });
 
-    it("should connect to legacy AugmintToken contract");
+    it("should connect to supported legacy AugmintToken contracts", () => {
+        const legacyAddresses = Augmint.constants.SUPPORTED_LEGACY_AUGMINTTOKENS[augmint.deployedEnvironment.name];
+
+        const legacyContracts = augmint.getLegacyTokens(legacyAddresses);
+
+        const connectedAddresses = legacyContracts.map(lm => lm.address.toLowerCase());
+        const lowerCaseLegacyAddresses = legacyAddresses.map(addr => addr.toLowerCase());
+
+        assert.deepEqual(connectedAddresses, lowerCaseLegacyAddresses);
+    });
 });
