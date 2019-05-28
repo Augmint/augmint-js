17bfc10944002961f797a68c8fa780375ac9350d
juli_e <julia.ero@euedge.com>
2019-05-28 16:27:27 +0200

    generate source in sh

--- generateDocs.sh
+++ generateDocs.sh
@@ -1,10 +1,22 @@
+#!/bin/sh
+
 cat >./sourcefile-map.json <<EOF
 [
     {
       "pattern": "^",
-      "replace":   "${REPOSITORY_URL}/tree/${BRANCH}/src"
+      "replace":   "${REPOSITORY_URL}/tree/${BRANCH}/src/"
     }
 ]
 EOF
 
-typedoc --out ./dist/docs ./src --mode modules --target ES6 --tsconfig ./tsconfig.json --exclude node_modules --ignoreCompilerErrors --excludeExternals --excludePrivate --excludeNotExported --sourcefile-url-map sourcefile-map.json
+typedoc \
+--out ./dist/docs ./src \
+--mode modules \
+--target ES6 \
+--tsconfig ./tsconfig.json \
+--exclude node_modules \
+--ignoreCompilerErrors \
+--excludeExternals \
+--excludePrivate \
+--excludeNotExported \
+--sourcefile-url-map sourcefile-map.json
