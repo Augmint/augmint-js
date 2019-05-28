110d36475fbfaa191ffe249ead1c4a970f9b2c9a
juli_e <julia.ero@euedge.com>
2019-05-28 13:55:20 +0200

    play with sourcefile generation

---
+++ generateDocs.sh
@@ -0,0 +1,10 @@
+cat >./sourcefile-map.json <<EOF
+[
+    {
+      "pattern": "^",
+      "replace":   "${REPOSITORY_URL}/tree/${BRANCH}/src"
+    }
+]
+EOF
+
+typedoc --out ./dist/docs ./src --mode modules --target ES6 --tsconfig ./tsconfig.json --exclude node_modules --ignoreCompilerErrors --excludeExternals --excludePrivate --excludeNotExported --sourcefile-url-map sourcefile-map.json
