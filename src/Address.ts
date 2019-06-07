e7095e39c56a8d4fd8ba9c4eeba806a505c46f6f
robi <robert.szaloki@euedge.com>
2019-06-07 17:59:01 +0200

    basic address type

---
+++ src/Address.ts
@@ -0,0 +1,17 @@
+import BN from "bn.js";
+
+export class Address {
+    public static parse(address: string): Address {
+        return new Address(new BN(address))
+    }
+
+    public readonly address: BN;
+
+    constructor(address: BN) {
+        this.address = address;
+    }
+
+    public toString() {
+        return '0x' + this.address.toString(16).padStart(40,"0");
+    }
+}
