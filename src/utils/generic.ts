9b149909cb3a6b0a104decd1c9df701507c2f4bc
Treer Zoltán <treer@euedge.com>
2019-08-16 16:03:36 +0200

    utility function to map values of a Map

--- src/utils/generic.ts
+++ src/utils/generic.ts
@@ -1,8 +1,13 @@
-export { flatten, collectPromises };
+export { flatten, collectPromises, mapMap };
 
 function flatten<T>(arr: T[][]): T[] {
     return ([] as T[]).concat(...arr);
 }
+
 function collectPromises<S, T>(sources: S[], extractor: (s: S) => Promise<T[]>): Promise<T[]> {
     return Promise.all(sources.map(extractor)).then(flatten);
+}
+
+function mapMap<K, V1, V2>(map: Map<K, V1>, fun: (v: V1) => V2): Map<K, V2> {
+    return new Map(Array.from(map, ([key, val]) => [key, fun(val)]));
 }
