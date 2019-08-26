d7365f819b60ad4de7162ebf562735b286b55cbf
Treer Zoltán <treer@euedge.com>
2019-08-26 14:06:42 +0200

    export loan related types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { Augmint } from "./Augmint";
 import { Ratio, Tokens, Wei } from "./units";
+import { Loan } from "./Loan";
+import { LoanProduct } from "./LoanProduct";
+import { LoanManager } from "./LoanManager";
 import * as utils from "./utils/index";
 export { Augmint, utils };
 export { Wei, Tokens, Ratio };
+export { Loan, LoanProduct, LoanManager };
